eb0ba88efb30f3d01e044fbf060b90f103508c50
kbyla <abdelhamid.kbyla@uit.ac.ma>
2020-05-22 04:25:38 +0200

    simplefolio

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,25 +2,25 @@ import uuidv1 from 'uuid/v1';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
+  title: '', // e.g: 'abdelhamid kbyla | Developer'
   lang: '', // e.g: en, es, fr, jp
   description: '', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
+  title: 'hello i am a begener developer',
+  name: 'abdelhamid kbyla',
+  subtitle: 'i can creat for you alsome websites with reasnable price',
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
+  img: 'kbyla.jpg',
+  paragraphOne: 'i have taken alot of programming course in the university as a physics major and also in online courses like udemy',
+  paragraphTwo: 'the reason why you should chose me because i love alot this job and i am always developping my selfe',
+  paragraphThree: 'finaly takes for taking time to read about me',
   resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
 };
 
@@ -29,7 +29,25 @@ export const projectsData = [
   {
     id: uuidv1(),
     img: 'project.jpg',
-    title: '',
+    title: 'simplefolio',
+    info: 'this a website to add simplefolio about you',
+    info2: 'very sipmple and easy',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+  {
+    id: uuidv1(),
+    img: 'project.jpg',
+    title: 'robofriends',
+    info: 'a simple websiete just for learning the basics of react',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+  {
+    id: uuidv1(),
+    img: 'project.jpg',
+    title: 'robofriends redux',
     info: '',
     info2: '',
     url: '',
@@ -38,16 +56,7 @@ export const projectsData = [
   {
     id: uuidv1(),
     img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: uuidv1(),
-    img: 'project.jpg',
-    title: '',
+    title: 'mindbraind',
     info: '',
     info2: '',
     url: '',
@@ -57,9 +66,9 @@ export const projectsData = [
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
+  cta: 'contact me for awsome service',
+  btn: 'email me',
+  email: 'abdelhamid.kbyla@uit.ac.ma',
 };
 
 // FOOTER DATA
@@ -90,5 +99,5 @@ export const footerData = {
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
